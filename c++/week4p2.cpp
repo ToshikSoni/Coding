ffe4995ebb957bf404cec591227d602022ac61c1
ToshikSoni <tsoni.kota@gmail.com>
2023-03-12 15:03:37 +0530

    Fourth

--- c++/week4p2.cpp
+++ c++/week4p2.cpp
@@ -2,13 +2,28 @@
 using namespace std;
 class Student
 {
+protected:
     int roll_no;
     string name;
+
+public:
+    Student() {}
+    Student(int r, char n)
+    {
+        roll_no = r;
+        name = n;
+    }
 };
 class Fees : protected Student
 {
     int fees;
+
 public:
+    Fees() {}
+    Fees(int f)
+    {
+        fees = f;
+    }
     void submitFees()
     {
         cout << "Enter fees amount: ";
@@ -20,10 +35,33 @@ public:
         cout << "Receipt has been generated for fees amount: " << fees << endl;
     }
 };
-class Result : public Student
+class Result : protected Student
 {
     int marks;
     char grade;
+
+public:
+    Result() {}
+    Result(int m)
+    {
+        marks = m;
+    }
+    char showgrade()
+    {
+        if (marks == 100)
+            grade = 'O';
+        else if (marks >= 80)
+            grade = 'A';
+        else if (marks >= 60)
+            grade = 'B';
+        else if (marks >= 40)
+            grade = 'C';
+        else if (marks >= 20)
+            grade = 'D';
+        else if (marks >= 0)
+            grade = 'E';
+        return grade;
+    }
 };
 int main()
 {
